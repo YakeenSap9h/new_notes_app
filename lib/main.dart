7d8f43c7b5fc2886f16ffe9997202baacb14833b
YakeenSap9h <yakeeny632@gmail.com>
2025-09-17 09:43:51 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:new_notes_app/views/notes_view.dart';
+
+void main() {
+  runApp(NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
+
+      home: const NotesView(),
+    );
+  }
+}
---
+++ lib/views/widgets/custom_app_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:new_notes_app/views/widgets/custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: const Row(
+        children: [
+          Text("Notes", style: TextStyle(color: Colors.white, fontSize: 28)),
+          Spacer(),
+          CustomSearchIcon(),
+        ],
+      ),
+    );
+  }
+}
